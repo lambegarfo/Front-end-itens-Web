23676fae0fb2b354f8a1640934ff87dccbe31568
lambegarfo <vitor_hugo.f.s@hotmail.com>
2022-10-24 20:19:55 -0300

    Ajuste de função

--- Treinos-HTML-CSS-JS/Formularios/Formulario-Simples/formulario-simples.js
+++ Treinos-HTML-CSS-JS/Formularios/Formulario-Simples/formulario-simples.js
@@ -1,42 +1,31 @@
 
 
+
+
 function validaformulario() {
 
-    let user = document.forms["formCadastro"]["usuario"].value;
-    let password = document.forms["formCadastro"]["senha"].value;
-    let confirm = document.forms["formCadastro"]["confirmarSenha"].value;
-    let email = document.forms["formCadastro"]["email"].value;
+    var user = document.forms["formCadastro"]["usuario"].value;
+    var password = document.forms["formCadastro"]["senha"].value;
+    var confirmSenha = document.forms["formCadastro"]["confirmarSenha"].value;
+    var email = document.forms["formCadastro"]["email"].value;
 
-    if(user == '') {
-        alert("Campo Usuário não preenchido.");
+    if(user == '' || password == '' || confirmSenha == '' || email == '') {
+        alert("Por favor preencha todos os campos.");
         return false;
+    } else{
+
+        let passLength = password.length;
+
+        if(passLength < 6){
+            alert("Senha curta demais.");
+            return false;
+        } else if(password != confirmSenha){
+            alert("A senha confirmada não é igual a senha proposta.");
+            return false;
+        }
     }
 
-    if(password == ''){
-        alert("Campo Senha não preenchido.");
-        return false;
-    }
-
-    if(confirm == ''){
-        alert("Campo Confirmar Senha não preenchido.");
-        return false;
-    }
-
-    let passLength = password.length;
-
-    if(passLength < 6){
-        alert("Senha curta demais.");
-        return false;
-    }
-
-    if(password != confirm){
-        alert("A senha confirmada não é igual a senha proposta.");
-        return false;
-    }
-
-    if(email == ''){
-        alert("Campo E-mail não preenchido.");
-        return false
-    }
-
+    console.log('user');
+    alert("Confirma seus dados ?");
 }
+
