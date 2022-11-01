09952d695157bb8c0178920c394331fdad59f0dc
lambegarfo <vitor_hugo.f.s@hotmail.com>
2022-10-31 22:32:11 -0300

    EventListener no button e ajuste da função

--- Treinos-HTML-CSS-JS/Random-Nator/pages/Cor/gerarCor.js
+++ Treinos-HTML-CSS-JS/Random-Nator/pages/Cor/gerarCor.js
@@ -1,19 +1,31 @@
 
-var r = 0;
-var g = 0;
-var b = 0;
 
-const redFixo = document.getElementById('redValue');
-const greenFixo = document.getElementById('greenValue');
-const blueFixo = document.getElementById('blueValue');
-const backgroundNovo = document.getElementById('areaGerador');
+function geraCorRGB() {
 
-function geraCor() {
+    const redFixo = document.getElementById('redValue');
+    const greenFixo = document.getElementById('greenValue');
+    const blueFixo = document.getElementById('blueValue');
     
     redFixo.value = Math.floor(Math.random()*255);
     greenFixo.value = Math.floor(Math.random()*255);
     blueFixo.value = Math.floor(Math.random()*255);
 
-    backgroundNovo.style.backgroundColor
+    var r = 0;
+    var g = 0;
+    var b = 0;
+
+    r = redFixo.value;
+    g = greenFixo.value;
+    b = blueFixo.value;
+
+
+    let corNova = "rgb(" + r + "," + g + "," + b +")";
+
+    const backgroundNovo = document.getElementsByClassName('areaGerador');
+    backgroundNovo[0].style.backgroundColor = corNova;
 
 }
+
+
+const botaoGerar = document.getElementById('gerarCor');
+botaoGerar.addEventListener('click', geraCorRGB);
